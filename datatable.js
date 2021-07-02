671190102f587dc5171419305ee1b05d81d47230
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2021-07-02 14:55:37 +0700

    Update datatable.js

--- datatable.js
+++ datatable.js
@@ -3,82 +3,96 @@
 
 function create_table(tag_id, tag_class, data) {
 
-    var div = document.getElementById(tag_id);
-    var table = document.createElement('table');
-    table.className = tag_class;
-    var thead = document.createElement('thead');
-    var row = document.createElement('tr');
+    if (Array.isArray(data)) {
 
-    data[0].forEach(function(trdata) {
+        var div = document.getElementById(tag_id);
+        var table = document.createElement('table');
+        table.className = tag_class;
+        var thead = document.createElement('thead');
+        var row = document.createElement('tr');
 
-        if (Array.isArray(trdata)) {
+        if (data[0].length !== 0) {
 
-            var th = document.createElement('th');
-            
-            trdata.forEach(function(attributes) {
+            data[0].forEach(function(trdata) {
 
-                var attr = document.createElement('b');
-                attr.className = attributes[1];
-                attr.appendChild(document.createTextNode(attributes[0]));
-                th.appendChild(attr);
-                
+                if (Array.isArray(trdata)) {
+        
+                    var th = document.createElement('th');
+                    
+                    trdata.forEach(function(attributes) {
+        
+                        var attr = document.createElement('b');
+                        attr.className = attributes[1];
+                        attr.appendChild(document.createTextNode(attributes[0]));
+                        th.appendChild(attr);
+                        
+                    });
+        
+                    row.appendChild(th);
+        
+                } else {
+        
+                    var th = document.createElement('th');
+                    var attr = document.createElement('b');
+                    attr.appendChild(document.createTextNode(trdata));
+                    th.appendChild(attr);
+                    row.appendChild(th);
+        
+                }
+        
             });
+        
+            thead.appendChild(row);
+            table.appendChild(thead);
 
-            row.appendChild(th);
+        }
+        
+        if (data[1].length !== 0) {
 
-        } else {
-
-            var th = document.createElement('th');
-            var attr = document.createElement('b');
-            attr.appendChild(document.createTextNode(trdata));
-            th.appendChild(attr);
-            row.appendChild(th);
+            var tbody = document.createElement('tbody');
+    
+            data[1].forEach(function(tr) {
+        
+                var row = document.createElement('tr');
+        
+                tr.forEach(function(trdata) {
+        
+                    if (Array.isArray(trdata)) {
+        
+                        var td = document.createElement('td');
+                        
+                        trdata.forEach(function(action) {
+        
+                            var act = document.createElement('a');
+                            act.href = action[1]
+                            act.className = action[2];
+                            act.appendChild(document.createTextNode(action[0]));
+                            td.appendChild(act);
+        
+                        });
+        
+                        row.appendChild(td);
+        
+                    } else {
+        
+                        var td = document.createElement('td');
+                        td.appendChild(document.createTextNode(trdata));
+                        row.appendChild(td);
+        
+                    }
+        
+                });
+        
+                tbody.appendChild(row);
+        
+            });
+        
+            table.appendChild(tbody);
 
         }
 
-    });
+        div.appendChild(table);
 
-    thead.appendChild(row);
-    table.appendChild(thead);
-    var tbody = document.createElement('tbody');
-
-    data[1].forEach(function(tr) {
-
-        var row = document.createElement('tr');
-
-        tr.forEach(function(trdata) {
-
-            if (Array.isArray(trdata)) {
-
-                var td = document.createElement('td');
-                
-                trdata.forEach(function(action) {
-
-                    var act = document.createElement('a');
-                    act.href = action[1]
-                    act.className = action[2];
-                    act.appendChild(document.createTextNode(action[0]));
-                    td.appendChild(act);
-
-                });
-
-                row.appendChild(td);
-
-            } else {
-
-                var td = document.createElement('td');
-                td.appendChild(document.createTextNode(trdata));
-                row.appendChild(td);
-
-            }
-
-        });
-
-        tbody.appendChild(row);
-
-    });
-
-    table.appendChild(tbody);
-    div.appendChild(table);
+    }
 
 }
